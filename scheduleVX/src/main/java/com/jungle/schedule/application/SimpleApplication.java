0eeec85fb953a65e4b164c7fa26bbcba4ac541c7
Jungle <dgJungle_c@163.com>
2022-04-22 22:39:12 +0800

    cors 配置

--- scheduleVX/src/main/java/com/jungle/schedule/application/SimpleApplication.java
+++ scheduleVX/src/main/java/com/jungle/schedule/application/SimpleApplication.java
@@ -1,5 +1,6 @@
 package com.jungle.schedule.application;
 
+import com.alibaba.fastjson.JSON;
 import com.jungle.schedule.core.ManagerInfo;
 import com.jungle.schedule.core.definition.RequestScheduleDefinition;
 import com.jungle.schedule.core.definition.SimpleScheduleDefinition;
@@ -8,17 +9,21 @@ import com.jungle.schedule.core.manager.SimpleScheduleManager;
 import com.jungle.schedule.enums.ScheduleType;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
+
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 
 public class SimpleApplication {
     public static Vertx vertx;
 
     public static void main(String[] args) {
-
         vertx = buildVertx();
         SimpleScheduleManager manager = new SimpleScheduleManager(vertx);
         vertx.getOrCreateContext().put("schedules_manager", manager);
@@ -32,18 +37,19 @@ public class SimpleApplication {
         HttpServerOptions serverOptions = new HttpServerOptions();
         HttpServer httpServer = vertx.createHttpServer(serverOptions);
         Router router = Router.router(vertx);
+        CorsHandler corsHandler = CorsHandler.create("*").allowedMethods(new HashSet<>(HttpMethod.values()));
+        router.route().handler(corsHandler);
         router.get("/schedule-info").handler(ctx -> {
             HttpServerResponse response = ctx.response();
             response.putHeader("content-type", "text/plain");
             ManagerInfo info = schedulesManager.getInfo();
-            response.end(info.toString());
+            response.end(JSON.toJSONString(info));
         });
 
         httpServer.requestHandler(router).listen(9965, res -> {
             if (res.succeeded()) {
                 System.out.println("Service start success!");
                 loadBaseSchedule(schedulesManager);
-
             } else {
                 System.out.println("Service start failed!" + res.cause().getMessage());
             }
